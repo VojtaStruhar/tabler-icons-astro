6ef3f3ae3e5eccd5398b2d3c48a39f104a5b07e9
Vojtěch Struhár <vojtechstruhar@gmail.com>
2022-12-05 14:55:50 +0100

    Export generation path fix

--- generate-index.sh
+++ generate-index.sh
@@ -1,9 +1,11 @@
 #!/bin/zsh
 
+ICONS_FOLDER="./icons-astro"
+
 # Erase file contents
 echo -n "" > index.js
 
-for FILE in $(ls icons-astro); do
+for FILE in $(ls $ICONS_FOLDER); do
 
     # Remove the suffix
     EXPORTNAME=${FILE%.astro}
@@ -11,5 +13,5 @@ for FILE in $(ls icons-astro); do
     # If filename starts with a digit, prefix export name with "Icon"
     if [[ $FILE =~ ^[0-9] ]] && EXPORTNAME="Icon${EXPORTNAME}"
 
-    echo "export { default as $EXPORTNAME } from '$FILE'" >> index.js 
+    echo "export { default as $EXPORTNAME } from '$ICONS_FOLDER/$FILE'" >> index.js 
 done
